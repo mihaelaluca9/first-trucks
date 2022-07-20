0fdd2fd1df1583d778d006547a8e2fec50965315
LEVI9\mihaela.luca <mihaela.luca@levi9.com>
2022-07-20 13:22:08 +0300

    added sdds to angular project

---
+++ first-trucks/src/main.ts
@@ -0,0 +1,21 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+//Import components and theme
+import { defineCustomElements, addTheme } from '@scania/components';
+import { theme as scania } from '@scania/theme-light';
+
+// Run function to initiate component
+defineCustomElements();
+
+// add theme
+addTheme(scania)
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
