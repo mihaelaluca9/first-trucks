f99ad4088bd33781cb5b99b063a3bece03a2d939
Bianca Mihaela Tizu <b.tizu@levi9.com>
2022-07-20 18:14:52 +0300

    Generated the auth module with login and logout components

--- first-trucks/src/app/app.module.ts
+++ first-trucks/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserLogoutComponent } from './user-logout/user-logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserLogoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ first-trucks/src/app/user-authentification/user-authentification.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserLogoutComponent } from './user-logout/user-logout.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserLogoutComponent,
+    UserLoginComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserAuthentificationModule { }
